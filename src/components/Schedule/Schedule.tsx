2a251d47e9d818f05c229851a547bede642f887a
dpalikova <65405865+dpalikova@users.noreply.github.com>
2024-09-30 17:29:41 +0300

    Add eslint disable line

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -116,6 +116,7 @@ export default function Schedule() {
 
   useEffect(() => {
     calculateDates();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [startDate, endDate]);
 
   const calculateDates = () => {
